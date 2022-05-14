1622be4b3ed031eb27794c61db7870800fb2ccbe
ravularohit <ravularohit5@gmail.com>
2022-05-14 13:33:33 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -3,5 +3,8 @@ module "s3-bucket" {
   version = "2.8.0"
   # insert required variables here
   bucket = "Rohit-ravula-my-s3-bucket"
-  
+  acl    = "private"
+
+  versioning = {
+    enabled = true
 }
