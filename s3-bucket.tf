a83932548354473d82c0b457be03f332f4bbdffd
ravularohit <ravularohit5@gmail.com>
2022-05-14 13:25:24 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,11 +1,6 @@
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-
+module "s3-bucket" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "2.8.0"
+  # insert required variables here
   bucket = "my-s3-bucket"
-  acl    = "private"
- 
-  versioning = {
-    enabled = true
-  }
-
 }
