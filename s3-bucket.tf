2dfac51c04b0b436100ea6bc08cd16441522354f
ravularohit <ravularohit5@gmail.com>
2022-05-14 13:35:26 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,7 +2,7 @@ module "s3-bucket" {
   source  = "terraform-aws-modules/s3-bucket/aws"
   version = "2.8.0"
   # insert required variables here
-  bucket = "Rohit-ravula-my-s3-bucket"
+  bucket = "ravularohit-my-s3-bucket"
   acl    = "private"
 
   versioning = {
